ac4f002998b707952c63f5e047c2931977823816
Tom Cunningham <tcunning@redhat.com>
2013-06-12 23:18:46 -0400

    Add windup scripts and pre-cooked windup report zips.

---
+++ labs/lab1/windup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Debug options
+#JAVA_OPTS="$JAVA_OPTS -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y"
+
+CURRENT_DIR=`pwd`
+
+# Setup WINDUP_HOME
+if [ "x$WINDUP_HOME" = "x" ]; then
+    WINDUP_HOME=/home/lab11/Libraries/windup-cli-0.7.0
+fi
+export WINDUP_HOME
+cd $WINDUP_HOME
+
+java $JAVA_OPTS -jar $WINDUP_HOME/windup-cli.jar -input $CURRENT_DIR/soa5_hello_world -output $CURRENT_DIR/lab1-report -javaPkgs org.jboss.soa.esb.samples.quickstart.helloworld -source true -fetchRemote true
+
